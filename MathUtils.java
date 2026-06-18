27a5a840ed16936f9d65658ee1fa6f19989ffdf7
Satya Prakash <95564166+SatyaisCoding@users.noreply.github.com>
2026-06-18 21:47:29 +0530

    Update MathUtils.java

---
+++ MathUtils.java
@@ -0,0 +1,12 @@
+public class MathUtils {
+    /**
+     * Returns the sum of two integers.
+     * 
+     * @param a the first integer
+     * @param b the second integer
+     * @return the sum of a and b
+     */
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
