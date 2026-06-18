f4a50f4ed325ad84c14d98e17152ba4aba0348be
Satya Prakash <95564166+SatyaisCoding@users.noreply.github.com>
2026-06-18 22:37:11 +0530

    Update MultiplyService.java

---
+++ MultiplyService.java
@@ -0,0 +1,12 @@
+public class MultiplyService {
+    /**
+     * This method multiplies two integers.
+     * 
+     * @param a The first integer to multiply.
+     * @param b The second integer to multiply.
+     * @return The product of a and b.
+     */
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+}
